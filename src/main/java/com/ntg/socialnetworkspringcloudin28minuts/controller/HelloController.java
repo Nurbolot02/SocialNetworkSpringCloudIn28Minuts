620d96461da47acd41df1c274cc24f34363fcb2d
Nurbolot <ngulamidinov@gmail.com>
2023-01-17 02:21:06 +0600

    add internationalization

--- src/main/java/com/ntg/socialnetworkspringcloudin28minuts/controller/HelloController.java
+++ src/main/java/com/ntg/socialnetworkspringcloudin28minuts/controller/HelloController.java
@@ -1,15 +1,28 @@
 package com.ntg.socialnetworkspringcloudin28minuts.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/hello")
+@RequiredArgsConstructor
 public class HelloController {
+    private final MessageSource messageSource;
     @GetMapping
     public String hello(@RequestParam(required = false) String name) {
         return String.format("Hello %s!", (name != null ? name : "world"));
+    }
+
+    @GetMapping("/inter")
+    public String helloInternationalization(@RequestParam(required = false) String name) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
